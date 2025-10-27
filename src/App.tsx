c8e8f79ffdd626b9bda2a9e88cfc26399cd83a3b
agimenez <agimenezg@svalero.com>
2025-10-27 18:00:00 +0100

    Añado estilos al marcador

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
   const [local, setLocal] = useState(0);
@@ -7,29 +6,52 @@ function App() {
 
   return (
     <>
-      <h1 style={{ marginBottom: "15px" }}>🏀 Marcador Reactivo</h1>
-
-      <div style={{ marginBottom: "20px", textAlign: "center" }}>
-        <h3>Local: {local}</h3>
-        <button onClick={() => setLocal(local + 1)}>+1</button>
-        <button onClick={() => setLocal(local + 2)}>+2</button>
-        <button onClick={() => setLocal(local + 3)}>+3</button>
-      </div>
-
-      <div style={{ marginBottom: "20px", textAlign: "center" }}>
-        <h3>Visitante: {visitante}</h3>
-        <button onClick={() => setVisitante(visitante + 1)}>+1</button>
-        <button onClick={() => setVisitante(visitante + 2)}>+2</button>
-        <button onClick={() => setVisitante(visitante + 3)}>+3</button>
-      </div>
-
-      <h2
+      <div
         style={{
-          color: "#000",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          backgroundColor: "#f5f5f5",
+          fontFamily: "system-ui, sans-serif",
         }}
       >
-        Resultado: {local} - {visitante}
-      </h2>
+        <div
+          style={{
+            backgroundColor: "#ffffff",
+            borderRadius: "12px",
+            padding: "40px",
+            boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
+            textAlign: "center",
+            width: "80%",
+            maxWidth: "360px",
+          }}
+        >
+          <h1 style={{ marginBottom: "15px" }}>🏀 Marcador Reactivo</h1>
+
+          <div style={{ marginBottom: "20px", textAlign: "center" }}>
+            <h3>Local: {local}</h3>
+            <button onClick={() => setLocal(local + 1)}>+1</button>
+            <button onClick={() => setLocal(local + 2)}>+2</button>
+            <button onClick={() => setLocal(local + 3)}>+3</button>
+          </div>
+
+          <div style={{ marginBottom: "20px", textAlign: "center" }}>
+            <h3>Visitante: {visitante}</h3>
+            <button onClick={() => setVisitante(visitante + 1)}>+1</button>
+            <button onClick={() => setVisitante(visitante + 2)}>+2</button>
+            <button onClick={() => setVisitante(visitante + 3)}>+3</button>
+          </div>
+
+          <h2
+            style={{
+              color: "#000",
+            }}
+          >
+            Resultado: {local} - {visitante}
+          </h2>
+        </div>
+      </div>
     </>
   );
 }
